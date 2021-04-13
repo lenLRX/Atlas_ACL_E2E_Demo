d73798320f94cb2c209608a3793338851260b665
lenlrx <545976176@qq.com>
2021-04-14 00:33:08 +0800

    support ctrl+c shutdown

--- src/signal_handler.h
+++ src/signal_handler.h
@@ -2,6 +2,7 @@
 #define __SIGNAL_HANDLER_H__
 
 #include <functional>
+#include <mutex>
 #include <vector>
 
 class SingalHandler {
@@ -9,14 +10,18 @@ public:
   using CbVec = std::vector<std::function<void(void)>>;
   SingalHandler() = default;
   ~SingalHandler() = default;
+  SingalHandler &operator=(const SingalHandler &) = delete;
+  SingalHandler(const SingalHandler &) = delete;
 
   static SingalHandler &GetInstancce();
   static void RegisterSignal();
   static void Register(std::function<void(void)> callback);
-  static CbVec GetCallBacks();
+  static CbVec &GetCallBacks();
+  static std::mutex &GetMtx();
 
 private:
   CbVec callbacks;
+  std::mutex cb_mtx;
 };
 
 #endif //__SIGNAL_HANDLER_H__
