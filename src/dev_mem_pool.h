395739860f050004a858971ba9fce41ae717040a
lenlrx <545976176@qq.com>
2022-02-04 17:21:03 +0800

    add devmem support for Atlas300I

--- src/dev_mem_pool.h
+++ src/dev_mem_pool.h
@@ -7,13 +7,16 @@
 
 #include "util.h"
 
+#define USE_MEM_POOL
+
 // a simple memory pool of device memory and dvpp memory
 
 struct DevMemPoolEntry {
   DevMemPoolEntry() = default;
-  DevMemPoolEntry(void *p, size_t sz) : dev_ptr_(p), size_(sz) {}
+  DevMemPoolEntry(void *p, size_t sz, int32_t dev_id) : dev_ptr_(p), size_(sz), dev_id_(dev_id) {}
   void *dev_ptr_;
   size_t size_;
+  int32_t dev_id_;
 };
 
 class DevMemPool {
@@ -67,9 +70,11 @@ private:
 
   void *AllocDevMemImpl(size_t size) {
     void *dev_mem = nullptr;
+    int32_t dev_id;
+    CHECK_ACL(aclrtGetDevice(&dev_id));
     {
       std::lock_guard<std::mutex> g(dev_mem_mtx_);
-      auto &list = dev_free_lists_[size];
+      auto &list = dev_free_lists_[dev_id][size];
       if (!list.empty()) {
         dev_mem = list.front().dev_ptr_;
         list.pop_front();
@@ -79,7 +84,7 @@ private:
     if (dev_mem == nullptr) {
       CHECK_ACL(aclrtMalloc(&dev_mem, size, ACL_MEM_MALLOC_HUGE_FIRST));
     }
-    DevMemPoolEntry entry(dev_mem, size);
+    DevMemPoolEntry entry(dev_mem, size, dev_id);
     {
       std::lock_guard<std::mutex> g(dev_mem_mtx_);
       dev_using_memory_[dev_mem] = entry;
@@ -92,15 +97,17 @@ private:
       std::lock_guard<std::mutex> g(dev_mem_mtx_);
       DevMemPoolEntry entry = dev_using_memory_.at(p);
       dev_using_memory_.erase(p);
-      dev_free_lists_[entry.size_].push_back(entry);
+      dev_free_lists_[entry.dev_id_][entry.size_].push_back(entry);
     }
   }
 
   void *AllocDvppMemImpl(size_t size) {
     void *dvpp_mem = nullptr;
+    int32_t dev_id;
+    CHECK_ACL(aclrtGetDevice(&dev_id));
     {
       std::lock_guard<std::mutex> g(dvpp_mem_mtx_);
-      auto &list = dvpp_free_lists_[size];
+      auto &list = dvpp_free_lists_[dev_id][size];
       if (!list.empty()) {
         dvpp_mem = list.front().dev_ptr_;
         list.pop_front();
@@ -110,7 +117,7 @@ private:
     if (dvpp_mem == nullptr) {
       CHECK_ACL(acldvppMalloc(&dvpp_mem, size));
     }
-    DevMemPoolEntry entry(dvpp_mem, size);
+    DevMemPoolEntry entry(dvpp_mem, size, dev_id);
     {
       std::lock_guard<std::mutex> g(dvpp_mem_mtx_);
       dvpp_using_memory_[dvpp_mem] = entry;
@@ -123,16 +130,16 @@ private:
       std::lock_guard<std::mutex> g(dvpp_mem_mtx_);
       DevMemPoolEntry entry = dvpp_using_memory_.at(p);
       dvpp_using_memory_.erase(p);
-      dvpp_free_lists_[entry.size_].push_back(entry);
+      dvpp_free_lists_[entry.dev_id_][entry.size_].push_back(entry);
     }
   }
 
   std::mutex dev_mem_mtx_;
   std::mutex dvpp_mem_mtx_;
   std::unordered_map<void *, DevMemPoolEntry> dev_using_memory_;
-  std::unordered_map<size_t, std::list<DevMemPoolEntry>> dev_free_lists_;
+  std::unordered_map<int32_t, std::unordered_map<size_t, std::list<DevMemPoolEntry>>> dev_free_lists_;
   std::unordered_map<void *, DevMemPoolEntry> dvpp_using_memory_;
-  std::unordered_map<size_t, std::list<DevMemPoolEntry>> dvpp_free_lists_;
+  std::unordered_map<int32_t, std::unordered_map<size_t, std::list<DevMemPoolEntry>>> dvpp_free_lists_;
 };
 
 #endif //__DEV_MEM_POOL_H__
