7943bba8d9c1cef1fa39cb80431520d3a8e03334
李睿昕 <545976176@qq.com>
2022-03-23 23:20:42 +0800

    Update ai1s_install_5.0.5.alpha001.sh

--- script/ai1s_install_5.0.5.alpha001.sh
+++ script/ai1s_install_5.0.5.alpha001.sh
@@ -4,11 +4,11 @@ sed -i "s@http://.*security.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/s
 apt update
 
 apt install -y build-essential libpcre3 libpcre3-dev zlib1g-dev openssl libssl-dev
-apt install -y cmake git libopencv-dev fonts-droid-fallback libfreetype6-dev libspdlog-dev nlohmann-json-dev python3-dev python3-sklearn python3-numpy python3-opencv python3.7 python3.7-dev libeigen3-dev
+apt install -y cmake git libopencv-dev fonts-droid-fallback libfreetype6-dev libspdlog-dev nlohmann-json-dev python3-dev python3-sklearn python3-numpy python3-opencv python3-pip python3.7 python3.7-dev libeigen3-dev
 
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip --upgrade
-pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-python3 -m pip install torch torchvision numpy --upgrade
+python3.6 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip --upgrade
+python3.6 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+python3.6 -m pip install torch torchvision numpy --upgrade
 
 wget --no-check-certificate https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/CANN/5.0.5.alpha001/Ascend-cann-toolkit_5.0.5.alpha001_linux-x86_64.run
 wget --no-check-certificate https://obs-9be7.obs.cn-east-2.myhuaweicloud.com/turing/resourcecenter/Software/AtlasI/A300-3010%201.0.12.2/A300-3010-npu-driver_21.0.3.2_linux-x86_64.run
